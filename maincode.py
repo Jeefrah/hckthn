df8521a207725098273e19daefb75161806978d9
Jeefrah <152088743+Jeefrah@users.noreply.github.com>
2024-05-18 16:27:04 +0300

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,9 +1,8 @@
-
 import subprocess
 
 target_ip_address = input("Please enter the  target ip adress:  ")
-speed = input("Please enter the attack speed of nmap ( between T0-T5 ) :  ")
-
+speed = input("Please enter the attack speed of nmap ( between -T0,-T5 ) :")  
+domain_address = input("Please enter the domain adress : ")
 
 def run_nmap(target_ip_address , speed):
     try:
@@ -18,5 +17,20 @@ def run_nmap(target_ip_address , speed):
     except Exception as e:
         print(f"nmap tool is occured: {e}")
 
+import subprocess
 
-run_nmap(target_ip_address,speed)
+def dirb_scan(domain_address):
+    try:
+        dirb_command = ["dirb", "https://" + domain_address]
+
+        result = subprocess.run(dirb_command, capture_output=True, text=True)
+
+    
+        print("Dirb tarama sonucu:")
+        print(result.stdout)
+
+    except Exception as e:
+        print(f"Dirb taraması başarısız oldu: {e}")
+
+run_nmap(target_ip_address, speed)
+dirb_scan(domain_address)
